49bb105378b4ea8a7dffea92c6117e0089e949d5
yazanalqawlaq-blip <yazanahmadalqawlaq@gmail.com>
2026-02-10 17:58:09 -0500

    Update 04_polishing.sh
    Formatting

--- scripts/04_polishing.sh
+++ scripts/04_polishing.sh
@@ -12,19 +12,19 @@ set -euo pipefail
 
 cd /scratch/yazanalq/Assignment-1---Binf6110
 
-ASSEMBLY="output_files/flye_assembly/assembly.fasta"
-FASTQ="SRR32410565.fastq.gz"
-MODEL="r1041_e82_400bps_sup_v5.0.0"
+RAW_ASM="output_files/flye_assembly/assembly.fasta"
+READS="SRR32410565.fastq.gz"
+MEDAKA_MODEL="r1041_e82_400bps_sup_v5.0.0"
 THREADS=16
 
-[[ -f "$FASTQ" ]]    || { echo "FASTQ not found" >&2; exit 1; }
-[[ -f "$ASSEMBLY" ]] || { echo "Assembly not found" >&2; exit 1; }
+[[ -f "$READS" ]]   || { echo "FASTQ not found" >&2; exit 1; }
+[[ -f "$RAW_ASM" ]] || { echo "Assembly not found" >&2; exit 1; }
 
 module load apptainer/1.2.4
 
-apptainer exec medaka_latest.sif medaka_consensus -m "$MODEL" \
-  -i "$FASTQ" \
-  -d "$ASSEMBLY" \
+apptainer exec medaka_latest.sif medaka_consensus -m "$MEDAKA_MODEL" \
+  -i "$READS" \
+  -d "$RAW_ASM" \
   -o output_files/medaka_out \
   -t "$THREADS"
 
